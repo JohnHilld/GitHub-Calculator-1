d0ef6bdcd5d5aea0606a0e77845a967b053963ce
JohnHilld <yfdsyfds0@ukr.net>
2022-11-02 19:16:07 +0200

    Add operation "subtraction"

--- src/app/Main.java
+++ src/app/Main.java
@@ -11,6 +11,10 @@ public class Main {
         System.out.println(c);
         int d = multiply(a, b);
         System.out.println(d);
+
+        //subtraction
+        int e = subtract(a, b);
+        System.out.println(e);
     }
 
     private static int add(int a, int b) {
@@ -19,5 +23,9 @@ public class Main {
 
     private static int multiply(int a, int b) {
         return a * b;
+    }
+
+    private static int subtract(int a, int b) {
+        return (a - b);
     }
 }
